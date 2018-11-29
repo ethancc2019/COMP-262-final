98fddcb731c64b7d5680bc73f0dce18ca207948c
ethancc2019 <ethan.collins335@myci.csuci.edu>
2018-11-29 13:39:49 -0800

    Added main

--- Flippy.py
+++ Flippy.py
@@ -26,6 +26,7 @@ idealFlip = "HHTHTHHHTHHHTHTH"
 actualFlip = ""
 counter = 0
 flag = 1
+probability = 0
 
 
 # pool = ThreadPool(4)
@@ -38,32 +39,30 @@ def compute_probability():
     size = len(idealFlip)
     print("do something damnit")
 
-def new_function():
-
 
 
 # make number of threads based on the length of the ideal flip
 def make_threads():
     pool = ThreadPool(len(idealFlip))
 
-
-while flag != 0:
+if __name__ == "__main__":
+    while flag != 0:
     # then = time.time()
-    for i in range(len(idealFlip)):
+        for i in range(len(idealFlip)):
         # Each iteration represents a independent flip of the coin
-        actualFlip += flip_coin(coinChoices)
+            actualFlip += flip_coin(coinChoices)
 
-    print("Actual flip = " + actualFlip + " Ideal flip = " + idealFlip)
+        print("Actual flip = " + actualFlip + " Ideal flip = " + idealFlip)
 
-    if actualFlip == idealFlip:
+        if actualFlip == idealFlip:
         # now = time.time()
-        flag = 0
+            flag = 0
 
-    counter += 1
-    actualFlip = ""
+        counter += 1
+        actualFlip = ""
 
 # diff = now - then
 # print(str(diff % 60))
-print("FOUND ON ITERATION: " + str(counter))
+    print("FOUND ON ITERATION: " + str(counter))
 
 
