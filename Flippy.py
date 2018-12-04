82275b614ec63fd787d34b3c2b1ddcc1741c25ba
ethancc2019 <ethan.collins335@myci.csuci.edu>
2018-12-04 13:53:52 -0800

    Experimental probability

--- Flippy.py
+++ Flippy.py
@@ -16,18 +16,31 @@
 # intuition behind coin flipping
 # https://math.stackexchange.com/questions/151262/looking-for-intuition-behind-coin-flipping-pattern-expectation
 
+
+# https://en.wikipedia.org/wiki/Binomial_distribution OMG WE NEED TO USE PROBABILITY MASS FUNCTION WHATTTT
+# Probability mass function --> https://en.wikipedia.org/wiki/Binomial_distribution
+# The probability of getting exactly k successes(in our case just one) in n trials is given by the probability mass function:
+
 import random
 import time
 import math
-from multiprocessing.dummy import Pool as ThreadPool
+import threading
+from scipy.stats import poisson
+import numpy as np
+import matplotlib.pyplot as plt
 
+# ***Global Variables***
 coinChoices = ["H", "T"]
-# number of flips
-idealFlip = "HHTHTTHT"
-counter = 0
-flag = 1
-probability = 0
+idealFlip = "HHTHTHHHTHHHTHTH"
+numOfFlips = 20  # We can change this easily depending on how many flips our experiment needs
 flip = ""
+
+counter = 0  # keep track of the iterations to divide by
+flag = 1  # flag to break the while loop
+probability = 0
+
+numOfHeads = 0
+numOfTails = 0
 
 
 # pool = ThreadPool(4)
@@ -36,37 +49,53 @@ def flip_coin(coins):
     return str(random.choice(coins))
 
 
-def compute_probability():
+def compute_probability():  # This function assumes that the probability of H or T is 1/2
+    global idealFlip
     size = len(idealFlip)
     return math.pow(0.5, size)
 
 
+def count_occurrence():  # Counts the number of H and T in the sequence
+    global numOfHeads
+    global numOfTails
+    for i in idealFlip:
+        if i == "H":
+            numOfHeads += 1
+        else:
+            numOfTails += 1
+
+def binomial_dist():
+    global numOfFlips
+    p = 0.5
+
+    s = np.random.binomial(numOfFlips,0.166)
+    print s
+
+
 # make number of threads based on the length of the ideal flip
-def make_threads():
-    pool = ThreadPool(len(idealFlip))
 
 
 if __name__ == "__main__":
+    binomial_dist()
 
-    probability = compute_probability()
     print("Initial probability of landing on the sequence: " + str(probability))
     actualProb = 0
+    probability = compute_probability()
     while flag != 0:
-        # then = time.time()
         for i in range(len(idealFlip)):
             # Each iteration represents a independent flip of the coin
             flip += flip_coin(coinChoices)
 
         print("Actual flip = " + flip + " Ideal flip = " + idealFlip)
         if counter != 0:
-            actualProb = (probability / counter)
+            actualProb = (probability / numOfFlips - 1)
         if flip == idealFlip:
-            # now = time.time()
             flag = 0
 
-        print("iteration number: " + str(counter) + " Probability: " + str(actualProb))
+        print("Iteration number: " + str(counter) + " Probability on this iteration: " + str(actualProb))
 
         counter += 1
+        # reset this string
         flip = ""
 
     # diff = now - then
---
+++ test_file.py
@@ -0,0 +1,63 @@
+import random
+import time
+import math
+import threading
+#from sympy import *import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+ideal = "HHTHTHHHTHHHTHTH"
+numOfFlips = 1000
+totalFlips = 0
+arrayOfProbability = []
+estimatedProbability = 0.0
+flp = ""
+y = 0
+n = 0
+
+
+def flip_sixteen():
+    global flp
+    for i in range(16):
+        flp += random.choice("H" "T")
+
+def compare_strings():
+    global ideal
+    global flp
+    global y,n
+    if ideal == flp:
+        y += 1
+        return
+    else:
+        n += 1
+        return
+
+def update_estimate():
+    global arrayOfProbability, estimatedProbability, totalFlips
+    theSum = 0
+    for i in arrayOfProbability:
+        theSum += i
+    estimatedProbability = (theSum / totalFlips) * 1000
+    print ("Estimated probability = " + str(estimatedProbability))
+
+if __name__ == "__main__":
+
+    for i in range(10000):
+
+        for j in range(numOfFlips):
+            flip_sixteen()
+            compare_strings()
+            flp = ""
+
+        probability = (float(y) / numOfFlips) * 100
+        arrayOfProbability.append(probability)
+        totalFlips += 1000
+        update_estimate()
+        # Reset values
+        probability = 0
+        y = 0
+        n = 0
+
+
+
