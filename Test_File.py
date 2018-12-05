38f031f6a331bc32ea9937e9aff3521c7df225f9
Ethan C Collins <ethan.collins335@myci.csuci.edu>
2018-12-05 13:51:28 -0800

    Changed the program to fit the specifications of our professor

--- Test_File.py
+++ Test_File.py
@@ -1,11 +1,68 @@
-from scipy.stats import poisson
-import numpy as np
+import random
+import time
+import math
+import threading
+#from sympy import *import numpy as np
 import matplotlib.pyplot as plt
-n = 1000
-prob = 0.3
-x = np.arange(0, n, 0.5)
-plt.plot(x, poisson.pmf(x, 600))
 
-plt.show()
-# Probability mass function --> https://en.wikipedia.org/wiki/Binomial_distribution The probability of getting
-# exactly k successes(in our case just one) in n trials is given by the probability mass function:
+
+
+ideal = "HHTHTHHH"
+numOfFlips = 1000
+totalFlips = 0
+arrayOfProbability = []
+estimatedProbability = 0.0
+flp = ""
+y = 0
+n = 0
+
+
+def flip_sixteen():
+   global flp
+   for i in range(16):
+       flp += random.choice("H" "T")
+
+def flip_n():
+    global flp
+    for i in range(len(ideal)):
+        flp += random.choice("H" "T")
+
+def compare_strings():
+   global ideal
+   global flp
+   global y,n
+   if ideal == flp:
+       y += 1
+       return
+   else:
+       n += 1
+       return
+
+def update_estimate():
+   global arrayOfProbability, estimatedProbability, totalFlips
+   theSum = 0
+   for i in arrayOfProbability:
+       theSum += i
+   estimatedProbability = (theSum / totalFlips) * 1000
+   print ("Estimated probability = " + str(estimatedProbability * 100))
+
+if __name__ == "__main__":
+
+   for i in range(1000):
+       for j in range(numOfFlips):
+           flip_n()
+           compare_strings()
+           flp = ""
+
+       probability = (float(y) / numOfFlips) * 100
+       arrayOfProbability.append(probability)
+       totalFlips += 1000
+       update_estimate()
+       # Reset values
+       probability = 0
+       y = 0
+       n = 0
+
+
+
+
