840242673c16f48b010d44aa1afdae87b3147c1b
Ethan C Collins <ethan.collins335@myci.csuci.edu>
2018-12-05 21:56:58 -0800

    Added some functionality for the threads. Created 5 threads that will flip a coin 1000 times(one 16 character sting per iteration) and adds it to a global array for all the sequences.

--- Test_File.py
+++ Test_File.py
@@ -1,68 +1,103 @@
 import random
+import sys
 import time
 import math
 import threading
-#from sympy import *import numpy as np
+# from sympy import *import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-ideal = "HHTHTHHH"
+ideal = "HHTHTHHHTHTHTHHH"
 numOfFlips = 1000
 totalFlips = 0
 arrayOfProbability = []
+arrayOfSequences = [sys.maxsize]  # Let's see how this goes lol
 estimatedProbability = 0.0
 flp = ""
-y = 0
-n = 0
+y = 0  # counter for number of matches
+n = 0  # counter for number of non matches
 
 
 def flip_sixteen():
-   global flp
-   for i in range(16):
-       flp += random.choice("H" "T")
+    global flp
+    for i in range(16):
+        flp += random.choice("H" "T")
+
 
 def flip_n():
-    global flp
+    flp = ""
     for i in range(len(ideal)):
         flp += random.choice("H" "T")
 
+    return flp
+
+
 def compare_strings():
-   global ideal
-   global flp
-   global y,n
-   if ideal == flp:
-       y += 1
-       return
-   else:
-       n += 1
-       return
+    global ideal
+    global flp
+    global y, n
+    if ideal == flp:
+        y += 1
+        return
+    else:
+        n += 1
+        return
+
 
 def update_estimate():
-   global arrayOfProbability, estimatedProbability, totalFlips
-   theSum = 0
-   for i in arrayOfProbability:
-       theSum += i
-   estimatedProbability = (theSum / totalFlips) * 1000
-   print ("Estimated probability = " + str(estimatedProbability * 100))
+    global arrayOfProbability, estimatedProbability, totalFlips
+    theSum = 0
+    for i in arrayOfProbability:
+        theSum += i
+    estimatedProbability = (theSum / totalFlips) * 1000
+
+
+def flip_add():
+    for j in range(1000):
+        flip = flip_n()
+        arrayOfSequences.append(flip)
+
 
 if __name__ == "__main__":
-
-   for i in range(1000):
-       for j in range(numOfFlips):
-           flip_n()
-           compare_strings()
-           flp = ""
-
-       probability = (float(y) / numOfFlips) * 100
-       arrayOfProbability.append(probability)
-       totalFlips += 1000
-       update_estimate()
-       # Reset values
-       probability = 0
-       y = 0
-       n = 0
+    t1 = threading.Thread(target=flip_add())
+    t2 = threading.Thread(target=flip_add())
+    t3 = threading.Thread(target=flip_add())
+    t4 = threading.Thread(target=flip_add())
+    t5 = threading.Thread(target=flip_add())
 
 
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
+    for j in arrayOfSequences:
+        print(j)
 
 
+
+    print(flp)
+    print(y)
+    print(n)
+# if __name__ == "__main__":
+#     t1 = threading.Thread(target=flip_sixteen())
+#     t1.start()
+#     for i in range(1000):
+#        for j in range(numOfFlips):
+#            flip_n()
+#            compare_strings()
+#            flp = ""
+#
+#        probability = (float(y) / numOfFlips) * 100
+#        arrayOfProbability.append(probability)
+#        totalFlips += 1000
+#        update_estimate()
+#        # Reset values
+#        probability = 0
+#        y = 0
+#        n = 0
