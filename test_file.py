7e1c19210a10153e86f509ad3c7a1658a234b5d3
ethancc2019 <ethan.collins335@myci.csuci.edu>
2018-12-06 14:35:06 -0800

    Progress: 12\06\18

--- test_file.py
+++ test_file.py
@@ -1,20 +1,19 @@
 import random
+import sys
 import time
 import math
 import threading
-#from sympy import *import numpy as np
-import matplotlib.pyplot as plt
-
-
+# from sympy import *import numpy as np
 
 ideal = "HHTHTHHHTHHHTHTH"
 numOfFlips = 1000
-totalFlips = 0
+totalFlips = 0 #keep track of the total number of flips
 arrayOfProbability = []
-estimatedProbability = 0.0
+arrayOfSequences = []  # sys.max size gives a capacity of 9223372036854775807 elements but we'll use 10,000 for testing
+estimatedProbability = 0.0 # this variable will update
 flp = ""
-y = 0
-n = 0
+y = 0  # counter for number of matches
+n = 0  # counter for number of non matches
 
 
 def flip_sixteen():
@@ -22,16 +21,14 @@ def flip_sixteen():
     for i in range(16):
         flp += random.choice("H" "T")
 
-def compare_strings():
-    global ideal
-    global flp
-    global y,n
-    if ideal == flp:
-        y += 1
-        return
-    else:
-        n += 1
-        return
+
+def flip_n():
+    flp = ""
+    for i in range(len(ideal)):
+        flp += random.choice("H" "T")
+
+    return flp
+
 
 def update_estimate():
     global arrayOfProbability, estimatedProbability, totalFlips
@@ -39,25 +36,65 @@ def update_estimate():
     for i in arrayOfProbability:
         theSum += i
     estimatedProbability = (theSum / totalFlips) * 1000
-    print ("Estimated probability = " + str(estimatedProbability))
+
+
+def flip_add():
+    for j in range(100000):
+        flip = flip_n()
+        arrayOfSequences.append(flip)
+
+
+def look_and_count(start, finish):
+    global ideal, flp, y, n, arrayOfSequences, numOfFlips
+    counter = 0
+    for i in range(start, finish):
+        if arrayOfSequences[counter] == ideal:
+            y += 1
+        else:
+            n += 1
+        if i != 0 and i % numOfFlips == 0:
+            probability = (float(y) / i)
+            arrayOfProbability.append(probability)
+
+        counter += 1
+
+
+
 
 if __name__ == "__main__":
+    now = time.time()
+    lock = threading.Lock()
+    t1 = threading.Thread(target=flip_add())
+    t2 = threading.Thread(target=flip_add())
+    t3 = threading.Thread(target=flip_add())
+    t4 = threading.Thread(target=flip_add())
+    t5 = threading.Thread(target=flip_add())
 
-    for i in range(10000):
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
 
-        for j in range(numOfFlips):
-            flip_sixteen()
-            compare_strings()
-            flp = ""
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
 
-        probability = (float(y) / numOfFlips) * 100
-        arrayOfProbability.append(probability)
-        totalFlips += 1000
-        update_estimate()
-        # Reset values
-        probability = 0
-        y = 0
-        n = 0
+    #look_and_count(0, len(arrayOfSequences))
+    t6 = threading.Thread(target=look_and_count, args=(0, len(arrayOfSequences))) # This thread sorts through the first half of our array
+    print("Sorting through the array")
+    t6.start()
+    t6.join()
 
+
+
+
+    then = time.time()
+
+    print("Number of matches: " + str(y))
+    print ("Number of non-matches: " + str(n))
+    print ("Completed in: " + str(then - now))
 
 
