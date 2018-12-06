7a7d707422c4966c45c374a7240933b050e99d50
Ethan C Collins <ethan.collins335@myci.csuci.edu>
2018-12-05 22:36:49 -0800

    Added some more threads that scans through the list and increments our counters

--- Test_File.py
+++ Test_File.py
@@ -6,11 +6,12 @@ import threading
 # from sympy import *import numpy as np
 import matplotlib.pyplot as plt
 
-ideal = "HHTHTHHHTHTHTHHH"
+ideal = "HHTHT"
 numOfFlips = 1000
 totalFlips = 0
 arrayOfProbability = []
-arrayOfSequences = [sys.maxsize]  # Let's see how this goes lol
+arrayOfSequences = [
+    10000]  # sys.max size gives a capacity of 9223372036854775807 elements but we'll use 10,000 for testing
 estimatedProbability = 0.0
 flp = ""
 y = 0  # counter for number of matches
@@ -31,18 +32,6 @@ def flip_n():
     return flp
 
 
-def compare_strings():
-    global ideal
-    global flp
-    global y, n
-    if ideal == flp:
-        y += 1
-        return
-    else:
-        n += 1
-        return
-
-
 def update_estimate():
     global arrayOfProbability, estimatedProbability, totalFlips
     theSum = 0
@@ -57,13 +46,21 @@ def flip_add():
         arrayOfSequences.append(flip)
 
 
+def look_and_count(start, finish):
+    global ideal, flp, y, n, arrayOfSequences
+    for i in range(start, round(finish)):
+        if arrayOfSequences[i] == ideal:
+            y += 1
+        else:
+            n += 1
+
+
 if __name__ == "__main__":
     t1 = threading.Thread(target=flip_add())
     t2 = threading.Thread(target=flip_add())
     t3 = threading.Thread(target=flip_add())
     t4 = threading.Thread(target=flip_add())
     t5 = threading.Thread(target=flip_add())
-
 
     t1.start()
     t2.start()
@@ -76,12 +73,21 @@ if __name__ == "__main__":
     t3.join()
     t4.join()
     t5.join()
-    for j in arrayOfSequences:
-        print(j)
 
+    # look_and_count(1, len(arrayOfSequences)/2)
+    #TODO: here
+    t6 = threading.Thread(target=look_and_count, args=(1, len(arrayOfSequences) - 1))
+    print("Sorting through the first half of the array")
+    t6.start()
+    t6.join(10)
+    pos = len(arrayOfSequences) / 2
+    t7 = threading.Thread(target=look_and_count, args=(pos + 1, len(arrayOfSequences) - 1))
+    print("Sorting through the second half of the array")
+    t7.start()
+    t7.join()
 
-
-    print(flp)
+    # t6.join()
+    # print(flp)
     print(y)
     print(n)
 # if __name__ == "__main__":
